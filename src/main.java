8b2df314c3eb67a32b93f8daab5c0553dae8de79
fran <javier.fran2602@gmail.com>
2025-10-06 20:35:17 -0600

    Prueba desde terminal

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 class Main {
 	public static void main(String[] args) {
-		System.out.println("¡Hola Mundo sirve??????????!");
+		String mensaje = "¡Hola Mundo!";
+		System.out.println(mensaje);
 	}
 }
