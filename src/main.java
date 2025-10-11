12666ba536fd23131e8e1fe915a3ea5943fa4d56
fran <javier.fran2602@gmail.com>
2025-10-11 16:06:43 -0600

    Clase archivo creada

--- src/main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-	public static void main(String[] args) {
-		String mensaje = "CR7, tronquito mayor, LDAnoooSAP";
-		// Aquí puedes agregar más lógica si es necesario
-		System.out.println(mensaje);
-	}
-}
