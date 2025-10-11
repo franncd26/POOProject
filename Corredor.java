5c98676162501e8c925040f82c35c2ef73731d0f
fran <javier.fran2602@gmail.com>
2025-10-11 16:56:55 -0600

    clase Corredor con atributos, getter y setters, faltan metodos

--- Corredor.java
+++ Corredor.java
@@ -1,3 +1,52 @@
+import java.time.LocalDate;
+
 public class Corredor {
+
+    // Atributos
+    private LocalDate fechaNacimiento;
+    private char sexo;
+    private String tipoSangre;
+    private String contactoEmergencia;
+
+    // Constructor
+    public Corredor(LocalDate fechaNacimiento, char sexo, String tipoSangre, String contactoEmergencia) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.tipoSangre = tipoSangre;
+        this.contactoEmergencia = contactoEmergencia;
+    }
+
+    // Getters
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+    public String getContactoEmergencia() {
+        return contactoEmergencia;
+    }
+
+    // Setters
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+    public void setContactoEmergencia(String contactoEmergencia) {
+        this.contactoEmergencia = contactoEmergencia;
+    }
+
+    //Metodos
+    @Override
+    public String toString() {
+    
     
 }
