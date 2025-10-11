07e04c535e5efaa344185e3ca2a54c4affdf6fa3
Dylan <dylanmendezzamora@gmail.com>
2025-10-11 15:28:26 -0600

    noooo

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 class Main {
 	public static void main(String[] args) {
-		String mensaje = "CR7, tronquito mayor, LDA<<SAP";
+		String mensaje = "CR7, tronquito mayor, LDA>>SAP";
 		// Aquí puedes agregar más lógica si es necesario
 		System.out.println(mensaje);
 	}
