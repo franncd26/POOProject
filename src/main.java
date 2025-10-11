90b855e0436d20df0d1ca464a89bef78962ab98b
fran <javier.fran2602@gmail.com>
2025-10-11 15:27:06 -0600

    doble facto

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 class Main {
 	public static void main(String[] args) {
-		String mensaje = "CR7, tronquito mayor";
+		String mensaje = "CR7, tronquito mayor, LDA<<SAP";
+		// Aquí puedes agregar más lógica si es necesario
 		System.out.println(mensaje);
 	}
 }
